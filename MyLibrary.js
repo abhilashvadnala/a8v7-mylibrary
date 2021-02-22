1098f52edfcc2c15725d29c9193bb6ea0b9c1f13
abhilashvadnala <abhivadnala@gmail.com>
2021-02-22 14:22:41 -0800

    added useLocalStorage hook

--- MyLibrary.js
+++ MyLibrary.js
@@ -3,31 +3,7 @@ import Insert from "./Insert";
 import FileUpload from "./FileUpload";
 import BookList from "./BookList";
 import { Row, Col } from "react-bootstrap";
-
-function useLocalStorage(key, initValue) {
-  const [storedValue, setStoredValue] = useState(() => {
-    try {
-      const item = window.localStorage.getItem(key);
-      return item ? JSON.parse(item) : initValue;
-    } catch (err) {
-      console.log(err);
-      return initValue;
-    }
-  });
-
-  const setValue = value => {
-    try {
-      const valueToStore =
-        value instanceof Function ? value(storedValue) : value;
-      setStoredValue(value);
-      window.localStorage.setItem(key, JSON.stringify(valueToStore));
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  return [storedValue, setValue];
-}
+import useLocalStorage from "./hooks/useLocalStorage";
 
 function MyLibrary(props) {
   const [id, setId] = useState("id", 1);
@@ -47,35 +23,52 @@ function MyLibrary(props) {
     );
   };
 
+  const [author, setAuthor] = useState({
+    name: "Abhilash",
+    year: new Date().getFullYear()
+  });
+
   return (
-    <Row id="library" className="justify-content-md-center">
-      <Col sm>
-        <Row className="row-padding">
-          <h1>
-            <span>📚</span> My Library <span>📚</span>
-          </h1>
-        </Row>
-        <Row className="row-padding">
-          <Col>
-            <Row className="row-padding">
-              <Insert setBookCB={addBook} />
-            </Row>
-            <hr />
-            <Row className="row-padding">
-              <FileUpload />
-            </Row>
-          </Col>
-        </Row>
-        <Row
-          className=" row-padding"
-          lg={{ span: 8, offset: 2 }}
-          sm={{ span: 10, offset: 1 }}
-        >
-          <BookList bookList={books} delBook={delBook} />
-          {books.length == 0 ? <h4>Such Empty Library 🥴</h4> : <></>}
-        </Row>
-      </Col>
-    </Row>
+    <>
+      <Row id="library" className="justify-content-md-center">
+        <Col sm>
+          <Row className="row-padding">
+            <h1>
+              <span>📚</span> My Library <span>📚</span>
+            </h1>
+          </Row>
+          <Row className="row-padding">
+            <Col>
+              <Row className="row-padding">
+                <Insert setBookCB={addBook} />
+              </Row>
+              <hr />
+              <Row className="row-padding">
+                <FileUpload />
+              </Row>
+            </Col>
+          </Row>
+          <Row
+            className=" row-padding"
+            lg={{ span: 8, offset: 2 }}
+            sm={{ span: 10, offset: 1 }}
+          >
+            <BookList bookList={books} delBook={delBook} />
+            {books.length == 0 ? <h4>Such Empty Library 🥴</h4> : <></>}
+          </Row>
+        </Col>
+      </Row>
+      <view
+        style={{
+          flex: 1,
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItem: "end"
+        }}
+      >
+        <pre>{`${author.name}, copyright ${author.year}`}</pre>
+      </view>
+    </>
   );
 }
 
